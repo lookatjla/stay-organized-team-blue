15142cf61c1e1692d70fe2a99f27b93eb91c5c0c
Jayla Young <jaylayoung1@gmail.com>
2023-12-07 12:13:01 -0500

    connected search button to onclick event

--- scripts/todos.js
+++ scripts/todos.js
@@ -1,14 +1,30 @@
 "use strict";
 
-let element = document.getElementById("userDropdown");
+// populate dropdown with user names from api
+let selectElement = document.getElementById("userDropdown");
 fetch("http://localhost:8083/api/users")
     .then(response => response.json())
     .then(data => {
         for (let i = 0; i < data.length; i++) {
+            // <option value="ID">NAME</option>
             let opt = document.createElement("option");
             opt.innerText = data[i].name;
-            opt.value = data[i].name;
-            element.append(opt);
+            opt.value = data[i].id;
+            selectElement.append(opt);
         }
     })
 
+
+// display users' tasks when user is clicked in dropdown
+window.onload = init;
+
+function init() {
+    const searchBtn = document.getElementById("searchButton");
+    searchBtn.onclick = onSearchBtnClicked;
+}
+
+function onSearchBtnClicked() {
+    const displayTask = document.getElementById("displayTask");
+    let message = "Search button has been clicked!"
+    displayTask.innerText = message;
+}
